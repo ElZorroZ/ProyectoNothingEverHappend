b011d93385c8295390c139768cad1adbaa93ca00
ElZorro <thiago2007crackz@gmail.com>
2025-04-17 15:22:36 -0300

    Dashboard+Mejoras de footer

--- Frontend/WEB/js/login.js
+++ Frontend/WEB/js/login.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("registroForm");
+  const form = document.getElementById("loginForm");
 
   if (!form) {
     console.error("No se encontró el formulario de login.");
     return;
   }
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Inicio de sesión exitoso");
+    window.location.href = "../DashboardWEB/dashboard.html"; 
+  });
 });
