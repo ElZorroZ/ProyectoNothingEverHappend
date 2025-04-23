174dc6abde04a3b2d14fa0255d2117ad7233ad67
ElZorro <thiago2007crackz@gmail.com>
2025-04-22 22:40:20 -0300

    Correcion de JS Register

--- Frontend/WEB/js/register.js
+++ Frontend/WEB/js/register.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = {
       nombre: formData.get("nombre"),
       apellido: formData.get("apellido"),
-      correo: formData.get("correo"),
+      email: formData.get("email"),
       contraseña: formData.get("contraseña"),
     };
 
@@ -27,12 +27,13 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       if (response.ok) {
+        const result = await response.text(); // en vez de .json()
         alert("Registro exitoso. Redirigiendo al login...");
         window.location.href = "../LoginWEB/login.html";
       } else {
-        const error = await response.json();
-        alert("Error al registrarse: " + (error.message || "Intente nuevamente."));
-      }
+        const error = await response.text(); // si devuelve texto o int también
+        alert("Error al registrarse: " + error);
+      }      
     } catch (error) {
       console.error("Error en la solicitud:", error);
       alert("Hubo un error al conectar con el servidor.");
